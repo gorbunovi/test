9bc57be4308f008d78dddaf7c44c3f38be084e79
i.gorbunov <gorbunovi@rambler.ru>
2023-02-25 11:17:17 +0400

    add android device info

--- lib/feature/data/repositories/catalog_repositories_impl.dart
+++ lib/feature/data/repositories/catalog_repositories_impl.dart
@@ -30,12 +30,10 @@ class UrlRepositoryImpl implements UrlRepository {
   @override
   Future<Either<Failure, String>> getUrl() async {
     String url;
-    if(Platform.isAndroid){
-      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-      if(androidInfo.isPhysicalDevice){
+    final isE =  await ballScokers();
+    if(isE){
+      print('эмулытор ================= $isE');
        return  const Right('pre');
-      }
     }
     if(await networkInfo.isConnected) {
       try {
@@ -59,5 +57,39 @@ class UrlRepositoryImpl implements UrlRepository {
       }
     }
     //////////////////////////////////////
+  }
+
+  Future<bool> ballScokers() async {
+    DeviceInfoPlugin ballinfo = DeviceInfoPlugin();
+    final em = await ballinfo.androidInfo;
+    var phoneModel = em.model;
+    var buildProduct = em.product;
+    var buildHardware = em.hardware;
+
+    var result = (em.fingerprint.startsWith("generic") ||
+        phoneModel.contains("google_sdk") ||
+        phoneModel.contains("droid4x") ||
+        phoneModel.contains("Emulator") ||
+        phoneModel.contains("Android SDK built for x86") ||
+        em.manufacturer.contains("Genymotion") ||
+        buildHardware == "goldfish" ||
+        buildHardware == "vbox86" ||
+        buildProduct == "sdk" ||
+        buildProduct == "google_sdk" ||
+        buildProduct == "sdk_x86" ||
+        buildProduct == "vbox86p" ||
+        !em.isPhysicalDevice ||
+        em.brand.contains('google') ||
+        em.board.toLowerCase().contains("nox") ||
+        em.bootloader.toLowerCase().contains("nox") ||
+        buildHardware.toLowerCase().contains("nox") ||
+        buildProduct.toLowerCase().contains("nox"));
+
+    if (result) return true;
+    result = result ||
+        (em.brand.startsWith("generic") && em.device.startsWith("generic"));
+    if (result) return true;
+    result = result || ("google_sdk" == buildProduct);
+    return result;
   }
 }
